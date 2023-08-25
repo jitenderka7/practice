e45bee9ce87ceabb01febbc8775f6b3a084d0ff6
ManishPersonal <manish.personal@HB2561-ManishSharma.local>
2023-08-25 22:11:14 +0530

    added arguments changes

--- argumentsSameParam.js
+++ argumentsSameParam.js
@@ -2,15 +2,29 @@ function add( ) {
     return arguments[0] * arguments[1];
 }
 
-
 const addResponse = add(20, 30);
 
 var addition = (a,a) => {
     return arguments[0] *  arguments[1];
 };
-
 const addResult = addition(4,5);
 
-console.log('first ' + addResponse);
+console.log('first : ' + addResponse);
 console.log('Second ' + addResult);
 
+
+
+// console.log(result.student());
+
+
+// console.log('second ' + [1,2]);
+// // cons
+// // const res = JSON.stringify(result);
+// // const parseResponse = JSON.parse(res);
+// // console.log(parseResponse.student);
+
+
+// const result1 = {
+//     student: () => 'Manish',
+//     rollNo: 23
+// }
