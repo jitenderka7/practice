4b56ca5f1f744a461acf263416ebbadb7238742d
ManishPersonal <manish.personal@HB2561-ManishSharma.local>
2023-08-25 22:13:54 +0530

    contruct functions

---
+++ constructFunctions.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    var abc = 'manish';
+} //it can make
+
+var addition = (a,b) => {
+    return a + b;
+} //it will not be used with new
+var a = 10;
+
+console.log(add());
+console.log(typeof NaN);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof undefined);
+console.log(typeof +'0'); // coercision ==> 0
+var rollnos = {}; 
+console.log(Array.isArray(rollnos));
+//Microsoft(JScript) => javascript
+//Javascript case sensitive => yes;
+var a = 10;
+var A = 20;
+console.log('a', a);
+console.log('A', A);
+// const res = new add(3,4);
+// const result = new addition(3,4);
+// console.log(res());
+// console.log(result);
