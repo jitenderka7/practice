23aab67eb00f464a7f9c87d365eea63bbacf03a3
ManishPersonal <manish.personal@HB2561-ManishSharma.local>
2023-08-25 21:42:03 +0530

    arguments in js function

---
+++ argumentsSameParam.js
@@ -0,0 +1,16 @@
+function add( ) {
+    return arguments[0] * arguments[1];
+}
+
+
+const addResponse = add(20, 30);
+
+var addition = (a,a) => {
+    return arguments[0] *  arguments[1];
+};
+
+const addResult = addition(4,5);
+
+console.log('first ' + addResponse);
+console.log('Second ' + addResult);
+
