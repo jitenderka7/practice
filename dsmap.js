ba7e36ae5b918e0342b905b4fa5452909d36d470
ManishPersonal <manish.personal@HB2561-ManishSharma.local>
2023-09-12 21:05:35 +0530

    map

---
+++ dsmap.js
@@ -0,0 +1,34 @@
+// const student = [{},{},{},{}]; //n
+// student[3];
+//===> baseaddress of student + 3 (n)
+
+// string
+// map
+// set
+// hash
+// tree
+// const customer = {a: '', b: '', c: ''} //nth value
+// student['c'] 
+
+const map1 = new Map();
+map1.set([1,2,3,4,5], [1,2,3,4,5] );
+// map1.set('b' , 14 );
+// console.log(map1);
+// console.log(map1.get('b'));
+// console.log(map1.size);
+// console.log(map1.delete('b'));
+// console.log(map1.size);
+console.log(map1);
+
+
+// var map = new Map([
+//     [5, 4],
+//     [7, 9]
+// ]);
+
+// //output:true
+// console.log(map instanceof Object); 
+
+// //output:false
+// var obj = new Object();
+// console.log(obj instanceof Map); 
